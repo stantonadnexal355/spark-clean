309e549e418c8bbfb04c304f588be339a10ac5cb
George Khananaev <georgekhananaev@gmail.com>
2026-03-06 01:16:32 +0200

    Initial Commit

---
+++ MacSimpleCleanup/MacSimpleCleanupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSimpleCleanupApp.swift
+//  MacSimpleCleanup
+//
+//  Created by George Khananaev on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MacSimpleCleanupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacSimpleCleanupTests/MacSimpleCleanupTests.swift
@@ -0,0 +1,16 @@
+//
+//  MacSimpleCleanupTests.swift
+//  MacSimpleCleanupTests
+//
+//  Created by George Khananaev on 3/6/26.
+//
+
+import Testing
+
+struct MacSimpleCleanupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
